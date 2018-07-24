243cc4e466c30d1f364144c5a3e5136736f56f0a
Marco-Malacara <marco.malacara7@gmail.com>
2018-07-24 11:10:53 -0500

    initial upload of project file

---
+++ Rock_Paper_Scissors/Rock_Paper_Scissors/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    class Game
+    {
+        //member variables
+
+        //constructor 
+
+        //member methods
+    }
+}
